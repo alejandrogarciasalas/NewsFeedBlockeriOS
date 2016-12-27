482ee245241846402ea2c175f873690f0732d7b8
alejandrogarciasalas <garciasalasalejandro@gmail.com>
2016-12-26 20:36:54 -0500

    blocks facebook newsfeed

---
+++ Content Blocker/ContentBlockerRequestHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ContentBlockerRequestHandler.swift
+//  Content Blocker
+//
+//  Created by Alejandro García Salas on 12/23/16.
+//  Copyright © 2016 Alejandro García Salas. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequest(with context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json"))!
+        
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+        
+        context.completeRequest(returningItems: [item], completionHandler: nil)
+    }
+    
+}
